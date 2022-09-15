687d54d4dfc70a00cb14518f781f5ea2aea58fc8
OrinhoAugusto <100860728+OrinhoAugusto@users.noreply.github.com>
2022-09-15 12:56:24 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+let arrei = []
+
+function calcular() {
+    document.getElementById('res').innerHTML = "";
+
+    const deslocamento = document.getElementById('deslocamento').value;
+    const tempo = document.getElementById('tempo').value;
+    let deslocamentoFloat = parseFloat(deslocamento);
+    let tempoFloat = parseFloat(tempo);
+    var velocidade;
+
+    if (deslocamento && tempo) {
+        velocidade = deslocamentoFloat / tempoFloat;
+        arrei.push(velocidade);
+        document.getElementById('resb').value = velocidade;
+    }
+
+    for (let i in arrei) {
+        document.getElementById('res').innerHTML += arrei[i] + "<br>";
+    }
+
+}
+
+function limpar() {
+    const deslocamento = document.getElementById('deslocamento').value='';
+    const tempo = document.getElementById('tempo').value='';
+    const resultado = document.getElementById('resb').value='';
+    const resultadob = document.getElementById('res').innerText ='';
+}
